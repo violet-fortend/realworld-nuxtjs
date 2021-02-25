5c1f8938b5a75369dd30b7dd583effbd9426df97
violetlovely <m13751158445_1@126.com>
2021-02-25 14:23:32 +0800

    发布部署-测试

---
+++ .nuxt/dist/server/4.js
@@ -0,0 +1,280 @@
+exports.ids = [4];
+exports.modules = {
+
+/***/ 28:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "h", function() { return getArticles; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "j", function() { return getFeedArticles; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "b", function() { return addFavorite; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "f", function() { return deleteFavorite; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "g", function() { return getArticle; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "c", function() { return createArticle; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "k", function() { return updateArticle; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "d", function() { return deleteArticle; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "i", function() { return getComments; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "a", function() { return addComment; });
+/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "e", function() { return deleteComment; });
+/* harmony import */ var _plugins_request__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(6);
+ //获取公共文章列表
+
+const getArticles = params => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: '/api/articles',
+    params
+  });
+}; //获取关注的用户文章列表
+
+const getFeedArticles = params => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: '/api/articles/feed',
+    params
+  });
+}; //添加点赞
+
+const addFavorite = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'POST',
+    url: `/api/articles/${slug}/favorite`
+  });
+}; //取消点赞
+
+const deleteFavorite = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'DELETE',
+    url: `/api/articles/${slug}/favorite`
+  });
+}; //获取文章详情
+
+const getArticle = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: `/api/articles/${slug}`
+  });
+}; //发表文章
+
+const createArticle = data => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'POST',
+    url: '/api/articles',
+    data
+  });
+}; //修改文章
+
+const updateArticle = (slug, data) => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'PUT',
+    url: `/api/articles/${slug}`,
+    data
+  });
+}; //删除文章
+
+const deleteArticle = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'DELETE',
+    url: `/api/articles/${slug}`
+  });
+}; //获取文章评论列表
+
+const getComments = slug => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'GET',
+    url: `/api/articles/${slug}/comments`
+  });
+}; //添加评论
+
+const addComment = (slug, data) => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'POST',
+    url: `/api/articles/${slug}/comments`,
+    data
+  });
+}; //删除评论
+
+const deleteComment = (slug, id) => {
+  return Object(_plugins_request__WEBPACK_IMPORTED_MODULE_0__[/* request */ "b"])({
+    method: 'DELETE',
+    url: `/api/articles/${slug}/comments/${id}`
+  });
+};
+
+/***/ }),
+
+/***/ 38:
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// CONCATENATED MODULE: ./node_modules/vue-loader/lib/loaders/templateLoader.js??vue-loader-options!./node_modules/vue-loader/lib??vue-loader-options!./pages/editor/index.vue?vue&type=template&id=11ff1244&
+var render = function () {var _vm=this;var _h=_vm.$createElement;var _c=_vm._self._c||_h;return _c('div',{staticClass:"editor-page"},[_vm._ssrNode("<div class=\"container page\"><div class=\"row\"><div class=\"col-md-10 offset-md-1 col-xs-12\"><form><fieldset><fieldset class=\"form-group\"><input type=\"text\" placeholder=\"Article Title\""+(_vm._ssrAttr("value",(_vm.article.title)))+" class=\"form-control form-control-lg\"></fieldset> <fieldset class=\"form-group\"><input type=\"text\" placeholder=\"What's this article about?\""+(_vm._ssrAttr("value",(_vm.article.description)))+" class=\"form-control\"></fieldset> <fieldset class=\"form-group\"><textarea rows=\"8\" placeholder=\"Write your article (in markdown)\" class=\"form-control\">"+_vm._ssrEscape(_vm._s(_vm.article.body))+"</textarea></fieldset> <fieldset class=\"form-group\"><input type=\"text\" placeholder=\"Enter tags\""+(_vm._ssrAttr("value",(_vm.new_tag)))+" class=\"form-control\"> <div class=\"tag-list\">"+(_vm._ssrList((_vm.article.tagList),function(tag){return ("<span class=\"tag-default tag-pill ng-binding ng-scope\"><i class=\"ion-close-round\"></i>"+_vm._ssrEscape("\n                  "+_vm._s(tag)+"\n                ")+"</span>")}))+"</div></fieldset> <button type=\"button\" class=\"btn btn-lg pull-xs-right btn-primary\">Publish Article</button></fieldset></form></div></div></div>")])}
+var staticRenderFns = []
+
+
+// CONCATENATED MODULE: ./pages/editor/index.vue?vue&type=template&id=11ff1244&
+
+// EXTERNAL MODULE: ./api/article.js
+var api_article = __webpack_require__(28);
+
+// CONCATENATED MODULE: ./node_modules/babel-loader/lib??ref--2-0!./node_modules/vue-loader/lib??vue-loader-options!./pages/editor/index.vue?vue&type=script&lang=js&
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+
+/* harmony default export */ var editorvue_type_script_lang_js_ = ({
+  //在路由匹配组件渲染之前会先执行中间件处理
+  middleware: 'authenticated',
+  name: 'EditorIndex',
+
+  data() {
+    return {
+      new_tag: '',
+      article: {
+        title: '',
+        description: '',
+        body: '',
+        tagList: []
+      }
+    };
+  },
+
+  async mounted() {
+    const slug = this.$route.params.slug;
+
+    if (slug) {
+      const {
+        data
+      } = await Object(api_article["g" /* getArticle */])(slug);
+      this.article = data.article;
+    }
+  },
+
+  methods: {
+    addTag() {
+      const index = this.article.tagList.findIndex(i => i === this.new_tag);
+
+      if (index === -1) {
+        this.article.tagList.push(this.new_tag);
+        this.new_tag = '';
+      }
+    },
+
+    deleteTag(tag) {
+      const index = this.article.tagList.findIndex(i => i === tag);
+      this.article.tagList.splice(index, 1);
+    },
+
+    async onSubmit() {
+      const slug = this.$route.params.slug || '';
+      const {
+        data
+      } = slug ? await Object(api_article["k" /* updateArticle */])(slug, {
+        article: this.article
+      }) : await Object(api_article["c" /* createArticle */])({
+        article: this.article
+      });
+      const {
+        article
+      } = data;
+      this.$router.push(`/article/${article.slug}`);
+    }
+
+  }
+});
+// CONCATENATED MODULE: ./pages/editor/index.vue?vue&type=script&lang=js&
+ /* harmony default export */ var pages_editorvue_type_script_lang_js_ = (editorvue_type_script_lang_js_); 
+// EXTERNAL MODULE: ./node_modules/vue-loader/lib/runtime/componentNormalizer.js
+var componentNormalizer = __webpack_require__(2);
+
+// CONCATENATED MODULE: ./pages/editor/index.vue
+
+
+
+function injectStyles (context) {
+  
+  
+}
+
+/* normalize component */
+
+var component = Object(componentNormalizer["a" /* default */])(
+  pages_editorvue_type_script_lang_js_,
+  render,
+  staticRenderFns,
+  false,
+  injectStyles,
+  null,
+  "23078f01"
+  
+)
+
+/* harmony default export */ var editor = __webpack_exports__["default"] = (component.exports);
+
+/***/ })
+
+};;
+//# sourceMappingURL=4.js.map
